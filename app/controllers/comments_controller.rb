636332f4eb358b7faa68990c587ab97fd4f511a7
felix_bonnier <felix_bonnier@yahoo.com>
2019-05-04 13:23:38 +0200

    adds correct instance variables for @article and @comment in comments controller, and reddirects after creation to article_path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to_article_path
+    redirect_to article_path(@article)
   end
 
   private 
